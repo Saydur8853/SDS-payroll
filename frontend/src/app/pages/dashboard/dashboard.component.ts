05d4ac0bccd5ac207bc11236224e83b8dc2873d2
saydur8853 <saydur774@gmail.com>
2026-05-11 17:05:20 +0600

    design improved

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,23 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { forkJoin } from 'rxjs';
 import { CompanyService } from '../../services/company.service';
+import { EmployeeService } from '../../services/employee.service';
+import { LookupService } from '../../services/lookup.service';
+import { ShiftService } from '../../services/shift.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent implements OnInit {
   companyCount = 0;
+  employeeCount = 0;
+  departmentCount = 0;
+  designationCount = 0;
+  shiftCount = 0;
   loading = true;
+  today = new Date();
 
-  constructor(private readonly companyService: CompanyService) {}
+  constructor(
+    private readonly companyService: CompanyService,
+    private readonly employeeService: EmployeeService,
+    private readonly lookupService: LookupService,
+    private readonly shiftService: ShiftService
+  ) {}
 
   ngOnInit(): void {
-    this.companyService.getAll().subscribe({
-      next: (companies) => {
-        this.companyCount = companies.length;
+    forkJoin({
+      companies: this.companyService.getAll(),
+      employees: this.employeeService.getAll({ page: 1, pageSize: 1 }),
+      departments: this.lookupService.getDepartments(),
+      designations: this.lookupService.getDesignations(),
+      shifts: this.shiftService.getAll()
+    }).subscribe({
+      next: (data) => {
+        this.companyCount = data.companies.length;
+        this.employeeCount = data.employees.totalCount ?? data.employees.items?.length ?? 0;
+        this.departmentCount = data.departments.length;
+        this.designationCount = data.designations.length;
+        this.shiftCount = data.shifts.length;
       },
       complete: () => {
         this.loading = false;
